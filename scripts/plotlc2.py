489abf7e5277784a82d3b6145089a332d755d213
Matthew Penny <penny1@lsu.edu>
2026-06-06 00:06:29 -0500

    Fix for change from source0 source1 to Source Source2

--- scripts/plotlc2.py
+++ scripts/plotlc2.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 import numpy as np
 import pandas as pd
 import sys
@@ -20,9 +21,11 @@ def mag(F,ms,fs):
 def magerr(F,e,ms,fs):
     return 2.5/np.log(10)*e/F
 
-data = pd.read_csv(sys.argv[1],sep=r'\s+',comment='#')
+filename = sys.argv[1]
 
-header = pd.read_csv(sys.argv[1],sep=r'\s+',header=None,comment=None,engine='python',nrows=50,index_col=False)
+data = pd.read_csv(filename,sep=r'\s+',comment='#')
+
+header = pd.read_csv(filename,sep=r'\s+',header=None,comment=None,engine='python',nrows=50,index_col=False)
 fsm = header[header.iloc[:,0]=='#fs:'].squeeze(axis=0)[1:].astype(float)
 event = header[header.iloc[:,0]=='#Event:'].squeeze(axis=0)[1:].astype(float)
 planet = header[header.iloc[:,0]=='#Planet:'].squeeze(axis=0)[1:].astype(float)
@@ -53,18 +56,23 @@ else:
 maglabels = ['W146','Z087','K213','W146','Z087','K213','W146','Z087','K213']
 
 
-plt.figure(figsize=(15,10))
+#fig,ax = plt.subplots(ncols=1,nrows=2)
+fig = plt.figure(layout="constrained",figsize=(20,12.5))
+fig.suptitle(filename)
+gs = GridSpec(3,1,figure=fig)
+ax1 = fig.add_subplot(gs[0:2,0])
+ax2 = fig.add_subplot(gs[2,0],sharex = ax1)
 
 #Find the baseline magnitude and source flux ratio for the reference observatory
 fs0 = fsm.iloc[match]
-print(type(fs0))
+#print(type(fs0))
 m0 = source.iloc[match] + 2.5*np.log10(fs0)
-print(m0,fs0,source.iloc[match])
-print(fsm)
+#print(m0,fs0,source.iloc[match])
+#print(fsm)
 
 #Plot the data
 for ii,i in enumerate(displayobs):
-    d = data[data['observatory_code']==i]
+    d = data[(data['observatory_code']==i) & (data['saturation_flag']==0)]
     #Find the source flux ratio and source magnitude
     fs=fsm.iloc[i]
     ms=source.iloc[i]
@@ -74,23 +82,39 @@ for ii,i in enumerate(displayobs):
     #Calculate magnification
     mu = A(d['measured_relative_flux'],fs)
     mutrue = A(d['true_relative_flux'],fs)
+    mufit = A(d['best_single_lens_fit'],fs)
     sigmu = d['measured_relative_flux_error']/fs
 
     #Calculate scaled magnitude
     mi = m0 - 2.5*np.log10(fs0*mu+1-fs0)
     mitrue = m0 - 2.5*np.log10(fs0*mutrue+1-fs0)
+    mifit = m0 - 2.5*np.log10(fs0*mufit+1-fs0)
     sigmi = 2.5/np.log(10) * sigmu/(fs0*mu+1-fs0) * fs0
 
 
     mask = (sigmi>0)
 
     #Plot with errorbars
-    plt.errorbar(d.loc[mask,'Simulation_time'],mi[mask],yerr=sigmi[mask],fmt='o',ms=4,
+    if ii==0:
+        ax1.plot(d.loc[mask,'Simulation_time'],mi[mask],'o',ms=4,
                  label=maglabels[i],color='C%d' % (ii))
+        ax2.plot(d.loc[mask,'Simulation_time'],mi[mask]-mifit[mask],'o',ms=4,
+                 label=maglabels[i],color='C%d' % (ii))
+    else:
+        ax1.errorbar(d.loc[mask,'Simulation_time'],mi[mask],yerr=sigmi[mask],fmt='o',ms=4,
+                     label=maglabels[i],color='C%d' % (ii))
+        ax2.errorbar(d.loc[mask,'Simulation_time'],mi[mask]-mifit[mask],yerr=sigmi[mask],fmt='o',ms=4,
+                     label=maglabels[i],color='C%d' % (ii))
+
+    ax1.autoscale_view()
+    ax2.autoscale_view()
+    ax1.autoscale(False)
+    ax2.autoscale(False)
     
-plt.xlabel('Time (days)')
-plt.ylabel(f'{maglabels[match]} magnitude')
-plt.legend()
+ax2.set_xlabel('Time (days)')
+ax1.set_ylabel(f'{maglabels[match]} magnitude')
+ax2.set_ylabel(f'Resid.')
+ax1.legend()
 
 
 #Plot the model lightcurves
@@ -98,7 +122,7 @@ plt.legend()
 #reference model lightcurve
 #print(dispobs,match)
 for i in [match]: #(dispobs,match)[dispobs==0]:
-    d = data[data['observatory_code']==i]
+    d = data[(data['observatory_code']==i) & (data['saturation_flag']==0)]
     #Find the source flux ratio and source magnitude
     fs=fsm.iloc[i]
     print(fs)
@@ -120,13 +144,17 @@ for i in [match]: #(dispobs,match)[dispobs==0]:
     #print(mitrue)
 
     #Plot with lines
-    plt.plot(d['Simulation_time'],mitrue,'-',color='k',alpha=0.5,zorder=10)
-    plt.plot(d['Simulation_time'],mifit,'--',color='C%d' % (i),alpha=0.5,zorder=11)
+    ax1.plot(d['Simulation_time'],mitrue,'-',color='k',alpha=0.5,zorder=10)
+    ax1.plot(d['Simulation_time'],mifit,'--',color='r',alpha=0.5,zorder=11)
+
+    ax2.plot(d['Simulation_time'],mitrue-mifit,'-',color='k',alpha=0.5,zorder=10)
+    ax2.plot(d['Simulation_time'],mifit-mifit,'--',color='r',alpha=0.5,zorder=11)
 
 
 #We're in magnitudes    
-plt.gca().invert_yaxis()
+ax1.invert_yaxis()
+ax2.invert_yaxis()
 
 
-
+plt.tight_layout()
 plt.show()
