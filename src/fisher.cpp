66970ea6f5d6aa94b17ccc2be7af64209905a125
Matthew Penny <penny1@lsu.edu>
2026-09-09 11:56:26 -0500

    Minor fisher changes

--- src/fisher.cpp
+++ src/fisher.cpp
@@ -18,6 +18,8 @@
 
 #define DEBUGVAR 0
 
+using namespace std;
+
 // Context struct for GSL derivative wrapper
 struct DerivContext {
     int param;
@@ -28,136 +30,207 @@ struct DerivContext {
 };
 
 // GSL derivative wrapper using context struct
-static double deriv_wrapper(double x, void* p) {
-    DerivContext* ctx = static_cast<DerivContext*>(p);
+static double deriv_wrapper(double x, void* p)
+{
+  DerivContext* ctx = static_cast<DerivContext*>(p);
+  
+  filekeywords* Paramfile = ctx->Paramfile;
+  event* Event = ctx->Event;
+  obsfilekeywords* World = ctx->World;
+  int param = ctx->param;
+  int idx = ctx->idx;
 
-    filekeywords* Paramfile = ctx->Paramfile;
-    event* Event = ctx->Event;
-    obsfilekeywords* World = ctx->World;
-    int param = ctx->param;
-    int idx = ctx->idx;
+  double logq = log10(Event->p_q[0]);
+  double logs = log10(Event->p_s0[0]);
+  double logtE = log10(Event->tE_r);
+  double logrs = log10(Event->rs);
+  double piEN = Event->piEN;
+  double piEE = Event->piEE;
 
-    double logq = log10(Event->params[QQ]);
-    double logs = log10(Event->params[SS]);
-    double logtE = log10(Event->tE_r);
-    double piEN = Event->piEN;
-    double piEE = Event->piEE;
+  double t0 = Event->t0, u0 = Event->u0;
 
-    double t0 = Event->t0, tE = pow(10, logtE), u0 = Event->u0, alpha = Event->alpha * TO_RAD;
-    double rs = Event->rs;
-
-    switch (param) {
-        case 0: t0 += x; break;
-        case 1: logtE += x; tE = pow(10, logtE); break;
-        case 2: u0 += x; break;
-        case 3: alpha += x; break;
-        case 4: logs += x; break;
-        case 5: logq += x; break;
-        case 6: rs = pow(10, log10(Event->rs) + x); break;
-        case 7: piEN += x; break;
-        case 8: piEE += x; break;
+  double rot = atan2(Event->p_y0[0],Event->p_x0[0]);
+  
+    
+  switch (param)
+    {
+    case 0: t0 += x; break;
+    case 1: logtE += x; break;
+    case 2: u0 += x; break;
+    case 3: rot += x; break;
+    case 4: logs += x; break;
+    case 5: logq += x; break;
+    case 6: logrs += x; break;
+    case 7: piEN += x; break;
+    case 8: piEE += x; break;
     }
 
-    double q = pow(10, logq);
-    double a = pow(10, logs);
-    double m1 = 1.0 / (1.0 + pow(10, logq));
-    double cosa = cos(alpha), sina = sin(alpha);
-    double origin = (1.0 - m1) * (-a);
+  double tE = pow(10, logtE);
+  double q = pow(10, logq);
+  double a = pow(10, logs);
+  double rs = pow(10, logrs); 
+  double m1 = 1.0 / (1.0 + q);
+  double origin = (1.0 - m1) * (-a);
 
-    int obsidx = Event->obsidx[idx];
-    int shiftedidx = idx - Event->nepochsvec[obsidx];
+  double cr = cos(-rot);
+  double sr = sin(-rot);
 
-    if (Paramfile->pllxMultiplyer && (param == 7 || param == 8 || param == 1)) {
-        for (int i = 0; i < Paramfile->numobservatories; i++)
-	  {
-	    Event->pllx[i].provide_observables_NE(piEN, piEE, tE, Event->thE);
-            Event->pllx[i].compute_tushifts();
-        }
+  int obsidx = Event->obsidx[idx];
+  int shiftedidx = idx - Event->nepochsvec[obsidx];
+
+  Event->pllx[obsidx].provide_observables_NE(piEN, piEE, tE, Event->thE);
+  double phi_pi = atan2(Event->pllx[obsidx].mulam_r, Event->pllx[obsidx].mubet_r);
+  double alpha = phi_pi - pi;
+  double cosa = cos(alpha), sina = sin(alpha);
+
+  if (Paramfile->pllxMultiplyer && (param == 7 || param == 8 || param == 1))
+    {
+      Event->pllx[obsidx].compute_tushifts(shiftedidx);
     }
 
-    double tt = (Event->epoch[idx] - t0) / tE;
-    double uu = u0;
-    if (Paramfile->pllxMultiplyer) {
-        tt += Event->pllx[obsidx].tshift[shiftedidx];
-        uu += Event->pllx[obsidx].ushift[shiftedidx];
-    }
+  double tt = (Event->epoch[idx] - t0) / tE;
+  double uu = u0;
+  if (Paramfile->pllxMultiplyer) {
+    tt += Event->pllx[obsidx].tshift[shiftedidx];
+    uu += Event->pllx[obsidx].ushift[shiftedidx];
+  }
 
-    double xs = tt * cosa - uu * sina + origin;
-    double ys = tt * sina + uu * cosa;
-    double lim_gamma=Paramfile->LD_GAMMA;
-    Event->vbm->a1=lim_gamma;
+  //double xs = tt * cosa - uu * sina + origin;
+  //double ys = tt * sina + uu * cosa;
 
-    return Event->vbm->BinaryMag2(a, q, xs, ys, rs);
+  double xl1 = origin * cos(rot+pi);
+  double yl1 = origin * q/(1.0+q) * sin(rot+pi);
+
+  double xs0 = uu * sina + tt * cosa + xl1;
+  double ys0 = -uu * cosa + tt * sina + yl1;
+
+
+  double xsi = cr*xs0 - sr*ys0;
+  double ysi = sr*xs0 + cr*ys0;
+  
+  double limb_gamma=Paramfile->LD_GAMMA;
+  Event->vbm->a1=limb_gamma;
+
+  double amp=Event->vbm->BinaryMag2(a, q, xsi, ysi, rs);
+
+  cout << Event->id << " " << param << " " << x << " " << xl1 << " " << yl1 << " " << xs0 << " " << ys0 << " " << xsi << " " << ysi << " " << rot << " " << alpha << " " << origin << " " << t0 << " " << tE << " " << u0 << " " << a << " " << q << " " << rs << " " << piEN << " " << piEE << " " << amp << endl;
+
+  return amp;
 }
 
 void fisherMatrix(filekeywords* Paramfile, event* Event,
-                  obsfilekeywords World[], slcat* Sources, slcat* Lenses) {
+                  obsfilekeywords World[], slcat* Sources, slcat* Lenses)
+{
+  if (Paramfile->verbosity>0) 
+    cout << "Calculating Fisher matrix for event " << Event->id << endl;
 
-    if (DEBUGVAR) {
-        std::cout << "Calculating Fisher matrix for event " << Event->id << std::endl;
+  const int Nparams = (Paramfile->pllxMultiplyer == 0 ? 7 : 9);
+  const int Nobsparams = 2 * Paramfile->numobservatories;
+  const int Ntotparams = Nparams + Nobsparams;
+
+  if (Paramfile->verbosity>0)
+    cout << "Nparams=" << Nparams << " Ntotparams=" << Ntotparams << endl;
+
+  //case 0: t0 += x; break;
+  //case 1: logtE += x; tE = pow(10, logtE); break;
+  //case 2: u0 += x; break;
+  //case 3: rot += x; break;
+  //case 4: logs += x; break;
+  //case 5: logq += x; break;
+  //case 6: rs = pow(10, log10(Event->rs) + x); break;
+  //case 7: piEN += x; break;
+  //case 8: piEE += x; break;
+  vector<double> step(Ntotparams);
+  for (int i = 0; i < Nparams; ++i)
+    {
+      //double base = (i < Event->params.size()) ? std::max(fabs(Event->params[i]), 1.0) : 1.0; //MP: I have no idea what this was doing
+      
+      if (i == 6) step[i] = 1.0e-3;
+      else if (i == 7 || i == 8) step[i] = 1.0e-4*Event->piE;
+      else if (i == 3) { step[i] = 1.0e-4; } // alpha is stored in degrees; multiply by TO_RAD so δalpha is in radians 
+      else step[i] = 1e-4;
     }
 
-    const int Nparams = (Paramfile->pllxMultiplyer == 0) ? 7 : 9;
-    const int Nobsparams = 2 * Paramfile->numobservatories;
-    const int Ntotparams = Nparams + Nobsparams;
+  Event->dF.clear();
+  Event->dF.resize(Ntotparams * Event->nepochs, 0.0);
 
-    std::vector<double> step(Ntotparams);
-    for (int i = 0; i < Nparams; ++i) {
-        double base = (i < Event->params.size()) ? std::max(fabs(Event->params[i]), 1.0) : 1.0;
-        if (i == 6) step[i] = 1e-2 * base;
-        else if (i == 7 || i == 8) step[i] = 1e-1 * base + 1e-5;
-        else if (i == 3) { step[i] = (1e-4 * base) * TO_RAD; } // alpha is stored in degrees; multiply by TO_RAD so δalpha is in radians 
-	else step[i] = 1e-4 * base;
-    }
+  for (int param = 0; param < Nparams; ++param)
+    {
+      DerivContext ctx{param, 0, Paramfile, Event, World};
+      gsl_function F;
+      F.function = &deriv_wrapper;
+      F.params   = &ctx;
 
-    Event->dF.clear();
-    Event->dF.resize(Ntotparams * Event->nepochs, 0.0);
+      if (Paramfile->verbosity>0)
+	cout << "Calculating derivatives for param " << param << endl;
 
-    for (int param = 0; param < Nparams; ++param) {
-        DerivContext ctx{param, 0, Paramfile, Event, World};
-        gsl_function F;
-        F.function = &deriv_wrapper;
-        F.params   = &ctx;
-
-	for (int idx = 0; idx < Event->nepochs; ++idx) {
-		ctx.idx = idx;
-		double result = 0.0, abserr = 0.0;
-		gsl_deriv_central(&F, 0.0, step[param], &result, &abserr);
-		int obsidx = Event->obsidx[idx];
-		double fs = Event->fs[obsidx];
-		double snr = fabs(result) / (abserr + 1e-12);
-		bool suppress = std::isnan(result) || std::isnan(abserr) ||
-                            ((param == 6) ? (snr < 1.0) : (snr < 3.0));
-		bool in_anomaly_window = fabs(Event->epoch[idx] - Event->t0) < 0.2 * Event->tE_r; //should we keep this for Earth mass planets?
-		int index = param * Event->nepochs + idx;
-		if (suppress && !in_anomaly_window) {
-			double interp = 0.0;
-			int count = 0;
-			for (int offset = 1; offset <= 3; ++offset) {
-				for (int j = -1; j <= 1; j += 2) {
-					int neighbor = idx + j * offset;
-					if (neighbor >= 0 && neighbor < Event->nepochs) {
-						double val = Event->dF[param * Event->nepochs + neighbor];
-						if (!std::isnan(val)) {
-							interp += val;
-							++count;
-						}
-					}
-				}
+      for (int idx = 0; idx < Event->nepochs; ++idx)
+	{
+	  ctx.idx = idx;
+	  double result = 0.0, abserr = 0.0;
+	  gsl_deriv_central(&F, 0.0, step[param], &result, &abserr);
+	  int obsidx = Event->obsidx[idx];
+	  double fs = Event->fs[obsidx];
+	  double snr = fabs(result) / (abserr + 1e-12);
+	  bool suppress = std::isnan(result) || std::isnan(abserr) ||
+	    ((param == 6) ? (snr < 1.0) : (snr < 3.0));
+	  bool in_anomaly_window = fabs(Event->epoch[idx] - Event->t0) < 0.2 * Event->tE_r; //should we keep this for Earth mass planets?
+	  int index = param * Event->nepochs + idx;
+	  if (suppress && !in_anomaly_window)
+	    {
+	      double interp = 0.0;
+	      int count = 0;
+	      for (int offset = 1; offset <= 3; ++offset)
+		{
+		  for (int j = -1; j <= 1; j += 2)
+		    {
+		      int neighbor = idx + j * offset;
+		      if (neighbor >= 0 && neighbor < Event->nepochs)
+			{
+			  double val = Event->dF[param * Event->nepochs + neighbor];
+			  if (!std::isnan(val))
+			    {
+			      interp += val;
+			      ++count;
+			    }
 			}
-			Event->dF[index] = (count > 0) ? (interp / count) : result * fs;
-		} else {
-			Event->dF[index] = result * fs;
+		    }
 		}
+	      Event->dF[index] = (count > 0) ? (interp / count) : result * fs;
+	    }
+	  else
+	    {
+	      Event->dF[index] = result * fs;
+	    }
+	} //end for idx
+
+      if (Paramfile->pllxMultiplyer && (param == 7 || param == 8 || param == 1))
+	{
+	  //reset parallax parameters back to nominal values if necessary
+	  for(int obsidx=0;obsidx<Paramfile->numobservatories;obsidx++)
+	    {
+	      Event->pllx[obsidx].provide_murel_h_lb(Event->murel_l,Event->murel_b,
+						     Event->piE, Event->thE);
+	      Event->pllx[obsidx].compute_tushifts();
+	    }
 	}
+      
+    } //end for param
+
+  if (Paramfile->verbosity>0)
+    cout << "Computing flux derivatives" << endl;
+      
+
+  for (int idx = 0; idx < Event->nepochs; ++idx)
+    {
+      int obsidx = Event->obsidx[idx];
+      int base = (Nparams + obsidx * 2) * Event->nepochs + idx;
+      Event->dF[base] = Event->Atrue[idx];
+      Event->dF[base + Event->nepochs] = (Event->Atrue[idx] - 1.0) / Event->fs[obsidx];
     }
 
-    for (int idx = 0; idx < Event->nepochs; ++idx) {
-        int obsidx = Event->obsidx[idx];
-        int base = (Nparams + obsidx * 2) * Event->nepochs + idx;
-        Event->dF[base] = Event->Atrue[idx];
-        Event->dF[base + Event->nepochs] = (Event->Atrue[idx] - 1.0) / Event->fs[obsidx];
-    }
+  if (Paramfile->verbosity>0)
+    cout << "All fisher derivatives calculated" << endl;
 
 }
 
--- src/fisherDetCuts.cpp
+++ src/fisherDetCuts.cpp
@@ -33,9 +33,9 @@ void detectionCuts(struct filekeywords* Paramfile, struct event *Event, struct o
 
   vector<string> parstrings;
   if(pllx) 
-	split(params_nopllx+params_pllx,parstrings);
+    split(params_nopllx+params_pllx,parstrings);
   else
-	split(params_nopllx,parstrings);
+    split(params_nopllx,parstrings);
 
   //dont bother if there was an error generating the lightcurve
   if(Event->lcerror) return;
@@ -55,89 +55,89 @@ void detectionCuts(struct filekeywords* Paramfile, struct event *Event, struct o
 
       //reset parallax parameters back to nominal values if necessary
       for(obsidx=0;obsidx<Paramfile->numobservatories;obsidx++)
-		{
-		  Event->pllx[obsidx].provide_murel_h_lb(Event->murel_l,Event->murel_b,
-												 Event->piE, Event->thE);
-		  Event->pllx[obsidx].compute_tushifts();
-		}
+	{
+	  Event->pllx[obsidx].provide_murel_h_lb(Event->murel_l,Event->murel_b,
+						 Event->piE, Event->thE);
+	  Event->pllx[obsidx].compute_tushifts();
+	}
 
-	  int nobs = int(Event->obsgroups[obsgroup].size());	  
-	  int nparams = nfixpar + 2*nobs;
+      int nobs = int(Event->obsgroups[obsgroup].size());	  
+      int nparams = nfixpar + 2*nobs;
 
-	  //Generate the header information
-	  for(int i=0;i<nparams;i++)
-		{
-		  ss.str(string(""));
-		  ss << "ObsGroup_" << obsgroup << "_";
-		  if(i<nfixpar)
-			{
-			  ss << parstrings[i] << "_err";
-			}
-		  else
-			{
-			  //for(grpidx=0;grpidx<nobs;grpidx++)
-			  grpidx = int((i-nfixpar)/2);
-			  obsidx=Event->obsgroups[obsgroup][grpidx];
+      //Generate the header information
+      for(int i=0;i<nparams;i++)
+	{
+	  ss.str(string(""));
+	  ss << "ObsGroup_" << obsgroup << "_";
+	  if(i<nfixpar)
+	    {
+	      ss << parstrings[i] << "_err";
+	    }
+	  else
+	    {
+	      //for(grpidx=0;grpidx<nobs;grpidx++)
+	      grpidx = int((i-nfixpar)/2);
+	      obsidx=Event->obsgroups[obsgroup][grpidx];
 			  
-			  if((i-nfixpar)%2==0)
-				{
-				  ss << "Fbase" << obsidx << "_err";
-				}
-			  else
-				{
-				  ss << "fs" << obsidx << "_err";
-					}
-			}
-		  Event->dataheader.push_back(ss.str());
+	      if((i-nfixpar)%2==0)
+		{
+		  ss << "Fbase" << obsidx << "_err";
 		}
+	      else
+		{
+		  ss << "fs" << obsidx << "_err";
+		}
+	    }
+	  Event->dataheader.push_back(ss.str());
+	}
 	  
 
       /* If finite source star effects important, fit FS lightcurve */
       if(abs(Event->umin) < 10*Event->rs && Event->PSPL[obsgroup].chisq>Paramfile->minChiSquared)
-		{
-		  Event->flag_needFS[obsgroup]=1;
-		  if(DEBUGVAR) cout << "Entering lightcurveFitter_FS" << endl;
-		  lightcurveFitter_FS(Paramfile, World, Event); 
-		  if(DEBUGVAR) cout << "Leaving lightcurveFitter_FS" << endl;
+	{
+	  Event->flag_needFS[obsgroup]=1;
+	  if(DEBUGVAR) cout << "Entering lightcurveFitter_FS" << endl;
+	  lightcurveFitter_FS(Paramfile, World, Event); 
+	  if(DEBUGVAR) cout << "Leaving lightcurveFitter_FS" << endl;
 		  
-		  //reset parallax parameters back to nominal values if necessary
-		  for(obsidx=0;obsidx<Paramfile->numobservatories;obsidx++)
-			{
-			  Event->pllx[obsidx].provide_murel_h_lb(Event->murel_l,Event->murel_b,
-													 Event->piE, Event->thE);
-			  Event->pllx[obsidx].compute_tushifts();
-			}
+	  //reset parallax parameters back to nominal values if necessary
+	  for(obsidx=0;obsidx<Paramfile->numobservatories;obsidx++)
+	    {
+	      Event->pllx[obsidx].provide_murel_h_lb(Event->murel_l,Event->murel_b,
+						     Event->piE, Event->thE);
+	      Event->pllx[obsidx].compute_tushifts();
+	    }
 
-		}
+	}
 
       if(Event->deterror) 
-		{
-		  fprintf(stderr,"\nDiscarding event %d (Failed fit FS, errval=%d) \n", 
-				  Event->id, Event->deterror);
-		}
+	{
+	  fprintf(stderr,"\nDiscarding event %d (Failed fit FS, errval=%d) \n", 
+		  Event->id, Event->deterror);
+	}
       else
-		{
-		  //did we detect it?
-		  if((!Event->flag_needFS[obsgroup] && Event->PSPL[obsgroup].chisq>Paramfile->minChiSquared)
-			 || (Event->flag_needFS[obsgroup] && Event->FSPL[obsgroup].chisq>Paramfile->minChiSquared))
-			Event->detected=1;
-		}
+	{
+	  //did we detect it?
+	  if((!Event->flag_needFS[obsgroup] && Event->PSPL[obsgroup].chisq>Paramfile->minChiSquared)
+	     || (Event->flag_needFS[obsgroup] && Event->FSPL[obsgroup].chisq>Paramfile->minChiSquared))
+	    Event->detected=1;
+	}
     }
 
   if(!Event->detected)
-	{
+    {
 
-	  for(obsgroup=0; obsgroup<int(Event->obsgroups.size()); obsgroup++)
-		{
-		  int nobs = int(Event->obsgroups[obsgroup].size());
-		  int nparams = nfixpar + 2*nobs;
-		  //Put dummy data in output
-		  for(int i=0;i<nparams;i++)
-			{
-			  Event->data.push_back(9999);
-			}
-		}
+      for(obsgroup=0; obsgroup<int(Event->obsgroups.size()); obsgroup++)
+	{
+	  int nobs = int(Event->obsgroups[obsgroup].size());
+	  int nparams = nfixpar + 2*nobs;
+	  //Put dummy data in output
+	  for(int i=0;i<nparams;i++)
+	    {
+	      Event->data.push_back(9999);
+	    }
 	}
+    }
   else
     {
 	  
@@ -147,122 +147,122 @@ void detectionCuts(struct filekeywords* Paramfile, struct event *Event, struct o
 
       //reset parallax parameters back to nominal values if necessary
       for(obsidx=0;obsidx<Paramfile->numobservatories;obsidx++)
-		{
-		  Event->pllx[obsidx].provide_murel_h_lb(Event->murel_l,Event->murel_b,
-												 Event->piE, Event->thE);
-		  Event->pllx[obsidx].compute_tushifts();
-		}
+	{
+	  Event->pllx[obsidx].provide_murel_h_lb(Event->murel_l,Event->murel_b,
+						 Event->piE, Event->thE);
+	  Event->pllx[obsidx].compute_tushifts();
+	}
 
       
 
       for(obsgroup=0; obsgroup<int(Event->obsgroups.size()); obsgroup++)
+	{
+	  Event->currentgroup=obsgroup;
+	  int nobs = int(Event->obsgroups[obsgroup].size());
+		  
+	  //Generate the header information
+	  ss.str(string(""));
+	  ss << Paramfile->outputdir << Paramfile->run_name << "_" 
+	     << Paramfile->instance << "_";
+	  if(Paramfile->choosefield>=0) ss << Paramfile->choosefield << "_";
+	  ss << Event->id << ".det.fm." << obsgroup;
+	  fmfname = ss.str();
+		  
+	  if(!Event->outputthis) fmfname=string("");
+		  
+	  ss.str(string(""));
+	  ss << "t0\tlog(tE)\tu0\talpha\tlog(s)\tlog(q)\tlog(rho)\t";
+	  if(pllx) ss << "piEN\tpiEE\t";
+	  ss << "{F0\tfs}";
+	  header = ss.str();
+		  
+	  ss.str(string(""));
+	  ss.precision(12);
+	  ss << scientific;
+	  ss << Event->t0 << "\t" << log10(Event->tE_r) << "\t" 
+	     << Event->u0 << "\t" << Event->alpha << "\t" 
+	     << log10(Event->p_s0[0]) << "\t" 
+	     << log10(Event->p_q[0]) << "\t" << log10(Event->rs) 
+	     << "\t";
+	  if(pllx) ss << Event->piEN << "\t" << Event->piEE << "\t";
+	  for(grpidx=0;grpidx<nobs;grpidx++)
+	    {
+	      obsidx = Event->obsgroups[obsgroup][grpidx];
+	      ss << 1.0 << "\t" << Event->fs[obsidx] << "\t";
+	    } //for grpidx
+	  values = ss.str();
+	  
+	  //Compile the differential vector
+	  vector<double> dF;
+	  vector<double> err;
+	  
+	  int nepochs=0;
+	  for(grpidx=0;grpidx<nobs;grpidx++)
+	    {
+	      obsidx=Event->obsgroups[obsgroup][grpidx];
+	      int startidx = Event->nepochsvec[obsidx];
+	      int endidx = Event->nepochsvec[obsidx+1];
+	      nepochs += (endidx - startidx);
+	    } //for grpidx
+	  
+	  if(Paramfile->verbosity>1) cout << "\nFisher matrix, group " << obsgroup << endl;
+		  
+	  //int nobs = int(Event->obsgroups[obsgroup].size());
+	  int nparams = nfixpar + 2*nobs;
+	  for(int param=0;param<nparams;param++)
+	    {
+	      int parobsidx = Event->obsgroups[obsgroup][(param-nfixpar)/2];
+	      int ofallparams = (param<nfixpar ? 
+				 param : 
+				 nfixpar + 2*parobsidx + (param-nfixpar)%2);
+	      int allshift = Event->nepochs*ofallparams;
+	      
+	      for(grpidx=0;grpidx<nobs;grpidx++)
 		{
-		  Event->currentgroup=obsgroup;
-		  int nobs = int(Event->obsgroups[obsgroup].size());
+		  obsidx=Event->obsgroups[obsgroup][grpidx];
 		  
-		  //Generate the header information
-		  ss.str(string(""));
-		  ss << Paramfile->outputdir << Paramfile->run_name << "_" 
-			 << Paramfile->instance << "_";
-		  if(Paramfile->choosefield>=0) ss << Paramfile->choosefield << "_";
-		  ss << Event->id << ".det.fm." << obsgroup;
-		  fmfname = ss.str();
+		  int startidx = Event->nepochsvec[obsidx];
+		  int endidx = Event->nepochsvec[obsidx+1];
+		  //if(param==0) nepochs += endidx - startidx;
+		  if(Paramfile->verbosity>2) cout << "nparams, nepochs, param, nobs, ofallparams, allshift, startidx, endidx, dFsize, Aerrsize = " << nparams << " " << nepochs << " " << param << " " << nobs << " " << ofallparams << " " << allshift << " " << startidx << " " << endidx << " " << Event->dF.size() << " " << Event->Aerr.size() << endl;
 		  
-		  if(!Event->outputthis) fmfname=string("");
-		  
-		  ss.str(string(""));
-		  ss << "t0\tlog(tE)\tu0\talpha\tlog(s)\tlog(q)\tlog(rho)\t";
-		  if(pllx) ss << "piEN\tpiEE\t";
-		  ss << "{F0\tfs}";
-		  header = ss.str();
-		  
-		  ss.str(string(""));
-		  ss.precision(12);
-		  ss << scientific;
-		  ss << Event->t0 << "\t" << log10(Event->tE_r) << "\t" 
-			 << Event->u0 << "\t" << Event->alpha << "\t" 
-			 << log10(Event->params[SS]) << "\t" 
-			 << log10(Event->params[QQ]) << "\t" << log10(Event->rs) 
-			 << "\t";
-		  if(pllx) ss << Event->piEN << "\t" << Event->piEE << "\t";
-		  for(grpidx=0;grpidx<nobs;grpidx++)
-			{
-			  obsidx = Event->obsgroups[obsgroup][grpidx];
-			  ss << 1.0 << "\t" << Event->fs[obsidx] << "\t";
-			} //for grpidx
-		  values = ss.str();
+		  for (int idx=startidx;idx<endidx;idx++)
+		    {
+		      dF.push_back(Event->dF[idx + allshift]);
+		      if(param==0) err.push_back(Event->Aerr[idx + allshift]);
+		    }
+		  if(Paramfile->verbosity>2) cerr << "df+Aerr copied for parameter " << param << endl;
+		} //for grpidx
+	    } //for param
 	  
-		  //Compile the differential vector
-		  vector<double> dF;
-		  vector<double> err;
-	  
-		  int nepochs=0;
-		  for(grpidx=0;grpidx<nobs;grpidx++)
-			{
-			  obsidx=Event->obsgroups[obsgroup][grpidx];
-			  int startidx = Event->nepochsvec[obsidx];
-			  int endidx = Event->nepochsvec[obsidx+1];
-			  nepochs += (endidx - startidx);
-			} //for grpidx
-	  
-		  if(Paramfile->verbosity>1) cout << "\nFisher matrix, group " << obsgroup << endl;
-		  
-		  //int nobs = int(Event->obsgroups[obsgroup].size());
-		  int nparams = nfixpar + 2*nobs;
-		  for(int param=0;param<nparams;param++)
-			{
-			  int parobsidx = Event->obsgroups[obsgroup][(param-nfixpar)/2];
-			  int ofallparams = (param<nfixpar ? 
-								 param : 
-								 nfixpar + 2*parobsidx + (param-nfixpar)%2);
-			  int allshift = Event->nepochs*ofallparams;
-	      
-			  for(grpidx=0;grpidx<nobs;grpidx++)
-				{
-				  obsidx=Event->obsgroups[obsgroup][grpidx];
-		  
-				  int startidx = Event->nepochsvec[obsidx];
-				  int endidx = Event->nepochsvec[obsidx+1];
-				  //if(param==0) nepochs += endidx - startidx;
-				  if(Paramfile->verbosity>2) cout << "nparams, nepochs, param, nobs, ofallparams, allshift, startidx, endidx, dFsize, Aerrsize = " << nparams << " " << nepochs << " " << param << " " << nobs << " " << ofallparams << " " << allshift << " " << startidx << " " << endidx << " " << Event->dF.size() << " " << Event->Aerr.size() << endl;
-		  
-				  for (int idx=startidx;idx<endidx;idx++)
-					{
-					  dF.push_back(Event->dF[idx + allshift]);
-					  if(param==0) err.push_back(Event->Aerr[idx + allshift]);
-					}
-				  if(Paramfile->verbosity>2) cerr << "df+Aerr copied for parameter " << param << endl;
-				} //for grpidx
-			} //for param
-	  
-		  if(Paramfile->verbosity>1)
-			{
-			  cout << "Computing fisher inversion for group " << obsgroup << " with " << nparams << " params and " << nepochs << " datapoints" << endl << endl;
-			}
+	  if(Paramfile->verbosity>1)
+	    {
+	      cout << "Computing fisher inversion for group " << obsgroup << " with " << nparams << " params and " << nepochs << " datapoints" << endl << endl;
+	    }
 	      
 
-		  if(Paramfile->verbosity>2)
-			{
-			  cerr << "dF.size " << dF.size() << endl;
-			  cerr << "err.size " << err.size() << endl;
-			  cerr << "nparams " << nparams << endl;
-			  cerr << "nepochs " << nepochs << endl;
-			}
+	  if(Paramfile->verbosity>2)
+	    {
+	      cerr << "dF.size " << dF.size() << endl;
+	      cerr << "err.size " << err.size() << endl;
+	      cerr << "nparams " << nparams << endl;
+	      cerr << "nepochs " << nepochs << endl;
+	    }
 
-		  cij = gsl_matrix_alloc(nparams,nparams);
+	  cij = gsl_matrix_alloc(nparams,nparams);
 		  
-		  fisherInversion(dF, err, nparams, nepochs, fmfname, header, values,
-						  &cij);
+	  fisherInversion(dF, err, nparams, nepochs, fmfname, header, values,
+			  &cij);
 
 
-		  //Put the data in output
-		  for(int i=0;i<nparams;i++)
-			{
-			  Event->data.push_back(sqrt(gsl_matrix_get(cij,i,i)));
-			}
+	  //Put the data in output
+	  for(int i=0;i<nparams;i++)
+	    {
+	      Event->data.push_back(sqrt(gsl_matrix_get(cij,i,i)));
+	    }
 		  
-		  gsl_matrix_free(cij);
+	  gsl_matrix_free(cij);
 			   
-		}
+	}
     }
 }
